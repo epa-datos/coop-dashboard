85802996a1b24e2a55410cff7a28b0a35560c19e
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-03-23 19:33:25 -0600

    add user to app models

---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    id: number;
+    email: string;
+    username?: string;
+    avatar_url: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at?: string;
+}
