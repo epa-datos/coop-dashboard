8518e7cac619a6fc7d7f5f338247333f4c491748
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-03-23 19:36:04 -0600

    add functionality to create-access component

--- src/app/modules/auth/pages/create-access/create-access.component.ts
+++ src/app/modules/auth/pages/create-access/create-access.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-create-access',
@@ -6,14 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-access.component.scss']
 })
 export class CreateAccessComponent implements OnInit {
-  user_email: string = 'user_email@test.com';
+  usermail: string;
+  reqState: number = 0;
+  errorMsg: string;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.usermail = this.route.snapshot.queryParamMap.get('usermail')
+      ? this.route.snapshot.queryParamMap.get('usermail')
+      : 'user_email@test.com';
   }
 
-  createPassword(newPassword) {
-    console.log('new password', newPassword);
+  singup(newPassword) {
+    this.reqState = 1;
+    this.userService.singup(this.usermail, newPassword)
+      .subscribe((newUser: User) => {
+
+        if (newUser.id) {
+          this.reqState = 2;
+          setTimeout(() => {
+            this.router.navigate(['/login']);
+          }, 6000);
+
+        } else {
+          this.reqState = 3;
+          this.errorMsg = 'Request failed. Please try again'
+        }
+      },
+        error => {
+          this.reqState = 3;
+          this.errorMsg = error?.error?.message ? error.error.message : error?.message;
+        }
+      )
   }
 }
