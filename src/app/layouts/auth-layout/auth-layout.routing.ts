22b0affd7523a67d0aca4b0321067c54d989bfa2
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-03-30 13:06:23 -0600

    remove unecessary code lines

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,16 +1,8 @@
 import { Routes } from '@angular/router';
-import { CreateAccessComponent } from 'src/app/modules/auth/pages/create-access/create-access.component';
-import { LoginComponent } from 'src/app/modules/auth/pages/login/login.component';
-import { ForgotPswComponent } from 'src/app/modules/auth/pages/forgot-psw/forgot-psw.component';
-import { ResetPswComponent } from 'src/app/modules/auth/pages/reset-psw/reset-psw.component';
 import { AuthComponent } from 'src/app/modules/auth/auth.component';
 import { AuthModule } from 'src/app/modules/auth/auth.module';
 
 export const AuthLayoutRoutes: Routes = [
-    // { path: 'create-access', component: CreateAccessComponent },
-    // { path: 'login', component: LoginComponent },
-    // { path: 'forgot-password', component: ForgotPswComponent },
-    // { path: 'reset-password', component: AuthModule },
     {
         path: '',
         component: AuthComponent,
