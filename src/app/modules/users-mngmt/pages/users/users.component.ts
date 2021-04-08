deb72f8f10700c7b246f8c1869a1938c6f9a35f1
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-08 17:34:12 -0500

    get invitations in user component and show invitation component

--- src/app/modules/users-mngmt/pages/users/users.component.ts
+++ src/app/modules/users-mngmt/pages/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersMngmtService } from '../../services/users-mngmt.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  private invitations: any[];
+  getReqSatus: number = 0;
 
-  constructor() { }
+  constructor(
+    private usersMngmtService: UsersMngmtService
+  ) { }
 
   ngOnInit(): void {
+    this.getInvitations();
   }
 
+  getInvitations() {
+    this.getReqSatus = 1;
+    this.usersMngmtService.getInvitations()
+      .subscribe(
+        (resp: any[]) => {
+          this.invitations = resp;
+          this.getReqSatus = 2;
+        },
+        error => {
+          this.getReqSatus = 3;
+          console.error(`[users.component]: ${error?.error?.message ? error.error.message : error?.message}`);
+        }
+      )
+  }
 }
