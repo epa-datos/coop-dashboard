61c7c60bc3729c56ee236f85cf052cad15294f4c
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-09 10:29:38 -0500

    show menu items based on user permissions

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsersMngmtService } from 'src/app/modules/users-mngmt/services/users-mngmt.service';
 import { UserService } from 'src/app/services/user.service';
 
 declare interface RouteInfo {
@@ -11,10 +12,6 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
   { path: '/dashboard/investment', title: 'Inversión', icon: 'text-primary', class: '', isForAdmin: false },
-  { path: '/dashboard/argentina', title: 'Argentina', icon: 'text-primary', class: '', isForAdmin: false },
-  { path: '/dashboard/colombia', title: 'Colombia', icon: 'text-primary', class: '', isForAdmin: false },
-  { path: '/dashboard/mexico', title: 'México', icon: 'text-primary', class: '', isForAdmin: false },
-  { path: '/dashboard/users', title: 'Administrar usuarios', icon: 'text-primary', class: '', isForAdmin: true },
   // { path: '/chart-js', title: 'chart.js', icon: 'ni-chart-bar-32 text-primary', class: '' },
   // { path: '/amcharts', title: 'amcharts', icon: 'ni-chart-pie-35 text-primary', class: '' },
   // { path: '/icons', title: 'Icons', icon: 'ni-planet text-blue', class: '' },
@@ -30,24 +27,69 @@ export const ROUTES: RouteInfo[] = [
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
 
   public menuItems: any[];
   public isCollapsed = true;
   public userIsAdmin: boolean;
+  public menuReqStatus: number = 0;
+  public errorMsg: string;
 
   constructor(
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private usersMngmtService: UsersMngmtService
   ) { }
 
-  ngOnInit() {
+  async ngAfterViewInit() {
     this.userIsAdmin = this.userService.isAdmin();
 
-    this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.menuItems = ROUTES;
     this.router.events.subscribe((event) => {
       this.isCollapsed = true;
     });
+
+    await this.getAvailableRoutes();
+
+    // Admin routes
+    const menuItem = {
+      path: '/dashboard/users',
+      title: 'Administrar usuarios',
+      icon: 'text-primary',
+      class: '',
+      isForAdmin: true
+    }
+    this.menuItems.push(menuItem);
+  }
+
+  ngOnInit() { }
+
+  getAvailableRoutes() {
+    this.menuReqStatus = 1;
+
+    return this.usersMngmtService.getCountries()
+      .toPromise()
+      .then((resp: any[]) => {
+        for (let country of resp) {
+          const menuItem = {
+            path: `/dashboard/${country.name.toLowerCase()}`,
+            title: country.name,
+            icon: 'text-primary',
+            class: '',
+            isForAdmin: false
+          }
+
+          this.menuItems.push(menuItem);
+          this.errorMsg && delete this.errorMsg;
+          this.menuReqStatus = 2;
+        }
+      })
+      .catch(error => {
+        this.errorMsg = error?.error?.message ? error.error.message : error?.message
+        console.error(this.errorMsg);
+        this.menuReqStatus = 3;
+        this.router.navigate(['dashboard/investment']);
+      });
   }
 
   logout() {
