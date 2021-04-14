b610ef42d07097d55f0a78abb6abeb81d8be9dbf
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-14 15:47:12 -0500

    use card-stat component y country component

--- src/app/modules/dashboard/pages/country/country.component.ts
+++ src/app/modules/dashboard/pages/country/country.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-country',
@@ -9,6 +8,49 @@ import { switchMap } from 'rxjs/operators';
 })
 export class CountryComponent implements OnInit {
   countryName: string;
+  stats: any[] = [
+    {
+      metricTitle: 'Inversión',
+      metricValue: 'USD 35,000',
+      icon: 'fas fa-wallet',
+      iconBg: '#172b4d'
+    },
+    {
+      metricTitle: 'Clicks',
+      metricValue: '280,0000',
+      subMetricTitle: 'CTR',
+      subMetricValue: '000',
+      icon: 'fas fa-hand-pointer',
+      iconBg: '#2f9998'
+
+    },
+    {
+      metricTitle: 'Bounce Rate',
+      metricValue: '12%',
+      subMetricTitle: 'Usuarios',
+      subMetricValue: '27000',
+      icon: 'fas fa-stopwatch',
+      iconBg: '#a77dcc'
+    },
+    {
+      metricTitle: 'Transacciones',
+      metricValue: '3,500',
+      subMetricTitle: 'CR',
+      subMetricValue: '000',
+      icon: 'fas fa-shopping-basket',
+      iconBg: '#f89934'
+    },
+    {
+      metricTitle: 'Revenue',
+      metricValue: '3,500',
+      subMetricTitle: 'ROAS',
+      subMetricValue: '000',
+      icon: 'fas fa-hand-holding-usd',
+
+      iconBg: '#fbc001'
+
+    }
+  ];
 
   constructor(private route: ActivatedRoute) { }
 
