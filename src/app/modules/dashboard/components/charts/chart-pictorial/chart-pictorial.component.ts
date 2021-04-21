2b2c7adfca4b2b5d9d53a16de0a729c0db04077f
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-21 16:30:30 -0500

    show dynamic icon in chart-pictorial component

--- src/app/modules/dashboard/components/charts/chart-pictorial/chart-pictorial.component.ts
+++ src/app/modules/dashboard/components/charts/chart-pictorial/chart-pictorial.component.ts
@@ -13,6 +13,8 @@ export class ChartPictorialComponent implements OnInit, AfterViewInit {
   @Input() data;
   @Input() value: string = 'value';
   @Input() category: string = 'category';
+  @Input() iconPath: string; // an svg icon path. If isnt't provide is necessary to use "iconType" input
+  @Input() iconType: string = 'human';
   chartID;
   loadStatus: number = 0;
 
@@ -36,15 +38,15 @@ export class ChartPictorialComponent implements OnInit, AfterViewInit {
   loadChart() {
     am4core.useTheme(am4themes_animated);
 
-    let iconPath = "M53.5,476c0,14,6.833,21,20.5,21s20.5-7,20.5-21V287h21v189c0,14,6.834,21,20.5,21 c13.667,0,20.5-7,20.5-21V154h10v116c0,7.334,2.5,12.667,7.5,16s10.167,3.333,15.5,0s8-8.667,8-16V145c0-13.334-4.5-23.667-13.5-31 s-21.5-11-37.5-11h-82c-15.333,0-27.833,3.333-37.5,10s-14.5,17-14.5,31v133c0,6,2.667,10.333,8,13s10.5,2.667,15.5,0s7.5-7,7.5-13 V154h10V476 M61.5,42.5c0,11.667,4.167,21.667,12.5,30S92.333,85,104,85s21.667-4.167,30-12.5S146.5,54,146.5,42 c0-11.335-4.167-21.168-12.5-29.5C125.667,4.167,115.667,0,104,0S82.333,4.167,74,12.5S61.5,30.833,61.5,42.5z"
+    let iconPath = this.iconPath ? this.iconPath : this.getIconPath();
 
     let chart = am4core.create(this.chartID, am4charts.SlicedChart);
 
     chart.data = this.data;
 
     let series = chart.series.push(new am4charts.PictorialStackedSeries());
-    series.dataFields.value = 'value';
-    series.dataFields.category = 'name';
+    series.dataFields.value = this.value;
+    series.dataFields.category = this.category;
     series.alignLabels = false;
 
     series.maskSprite.path = iconPath;
@@ -68,5 +70,19 @@ export class ChartPictorialComponent implements OnInit, AfterViewInit {
     // ];
 
     series.fillOpacity = 0.8;
+  }
+
+  getIconPath(): string {
+    let iconPath;
+    switch (this.iconType) {
+      case 'human':
+        iconPath = 'M53.5,476c0,14,6.833,21,20.5,21s20.5-7,20.5-21V287h21v189c0,14,6.834,21,20.5,21 c13.667,0,20.5-7,20.5-21V154h10v116c0,7.334,2.5,12.667,7.5,16s10.167,3.333,15.5,0s8-8.667,8-16V145c0-13.334-4.5-23.667-13.5-31 s-21.5-11-37.5-11h-82c-15.333,0-27.833,3.333-37.5,10s-14.5,17-14.5,31v133c0,6,2.667,10.333,8,13s10.5,2.667,15.5,0s7.5-7,7.5-13 V154h10V476 M61.5,42.5c0,11.667,4.167,21.667,12.5,30S92.333,85,104,85s21.667-4.167,30-12.5S146.5,54,146.5,42 c0-11.335-4.167-21.168-12.5-29.5C125.667,4.167,115.667,0,104,0S82.333,4.167,74,12.5S61.5,30.833,61.5,42.5z';
+        break;
+
+      case 'monitor':
+        iconPath = 'M119.93405,7.625H8.06595a5.646,5.646,0,0,0-5.55912,4.70525V93.53014a4.51389,4.51389,0,0,0,4.53957,4.3H53.68847l-.09043,3.6052a4.06956,4.06956,0,0,1-1.2809,2.7904l-5.45487,5.119a5.12215,5.12215,0,0,1-3.50509,1.38708h0A6.10922,6.10922,0,0,0,37.248,116.841v3.534H90.752v-3.534a6.10922,6.10922,0,0,0-6.10922-6.10922h0a5.12216,5.12216,0,0,1-3.50509-1.38708l-5.45487-5.119a4.06956,4.06956,0,0,1-1.2809-2.7904l-.09043-3.6052h46.64208a4.51389,4.51389,0,0,0,4.53957-4.3V12.33025A5.646,5.646,0,0,0,119.93405,7.625Zm-4.537,71.92959H12.603V17.74408H115.397Z'
+    }
+
+    return iconPath;
   }
 }
