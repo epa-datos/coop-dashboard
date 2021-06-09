808f100f7756ecc0414369500554162f87e95099
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-06-08 21:15:33 -0500

    update dashboard component

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationStart, Router, Event } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { AppStateService } from 'src/app/services/app-state.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,30 +13,36 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   title: string;
   routeSub: Subscription;
+  translateSub: Subscription;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private translate: TranslateService
+  ) {
     this.routeSub = this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationStart) {
         this.loadTitle(event.url);
       }
     })
+
+    this.translateSub = translate.stream('dashboard').subscribe(() => {
+      this.loadTitle(this.router.url);
+    });
   }
 
-  ngOnInit(): void {
-    this.loadTitle(this.router.url);
-  }
+  ngOnInit(): void { }
 
   loadTitle(route: string) {
     if (route.includes('/dashboard/main-region')) {
-      this.title = 'Visión general';
+      this.title = this.translate.instant('dashboard.overview');
     } else if (route.includes('/dashboard/country')) {
-      this.title = 'Visión general del país';
+      this.title = this.translate.instant('dashboard.overviewCountry');
     } else if (route.includes('/dashboard/retailer')) {
-      this.title = 'Visión general del retailer'
+      this.title = this.translate.instant('dashboard.overviewRetailer');
     } else if (route.includes('/dashboard/tools')) {
-      this.title = 'Otras herramientas';
+      this.title = this.translate.instant('dashboard.otherTools');
     } else if (route.includes('/dashboard/omnichat')) {
-      this.title = 'Análisis de sentimientos OmniChat';
+      this.title = this.translate.instant('dashboard.feelingsAnalysis');
     } else {
       delete this.title;
     }
@@ -42,5 +50,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.routeSub?.unsubscribe();
+    this.translateSub?.unsubscribe();
   }
 }
