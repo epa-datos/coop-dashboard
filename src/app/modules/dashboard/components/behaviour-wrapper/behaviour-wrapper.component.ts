4972fca06b5c567aa3443402ceb7f4061c56f153
María Isabel Álvarez Hernández <maria.alvarez9409@gmail.com>
2021-06-12 18:19:43 -0500

    add functionality to behaviour-wrapper component (#161)

--- src/app/modules/dashboard/components/behaviour-wrapper/behaviour-wrapper.component.ts
+++ src/app/modules/dashboard/components/behaviour-wrapper/behaviour-wrapper.component.ts
@@ -1,94 +1,114 @@
-import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CampaignInRetailService } from '../../services/campaign-in-retail.service';
+import { FiltersStateService } from '../../services/filters-state.service';
 
 @Component({
   selector: 'app-behaviour-wrapper',
   templateUrl: './behaviour-wrapper.component.html',
   styleUrls: ['./behaviour-wrapper.component.scss']
 })
-export class BehaviourWrapperComponent implements OnInit {
-
-  generalBehaviour = [
-    {
-      name: 'Porcentaje de salidas',
-      serie: [
-        { date: new Date(2021, 3, 15), value: 2500 },
-        { date: new Date(2021, 3, 16), value: 4700 },
-        { date: new Date(2021, 3, 17), value: 4600 },
-        { date: new Date(2021, 3, 18), value: 4700 },
-        { date: new Date(2021, 3, 19), value: 4500 },
-        { date: new Date(2021, 3, 20), value: 4300 },
-        { date: new Date(2021, 3, 21), value: 4400 }
-      ]
-    },
-    {
-      name: 'Porcentaje de rebote',
-      serie: [
-        { date: new Date(2021, 3, 15), value: 2000 },
-        { date: new Date(2021, 3, 16), value: 3500 },
-        { date: new Date(2021, 3, 17), value: 3200 },
-        { date: new Date(2021, 3, 18), value: 3600 },
-        { date: new Date(2021, 3, 19), value: 3000 },
-        { date: new Date(2021, 3, 20), value: 3400 },
-        { date: new Date(2021, 3, 21), value: 3000 }
-      ]
-    },
-    {
-      name: 'Número de páginas vistas',
-      serie: [
-        { date: new Date(2021, 3, 15), value: 4500 },
-        { date: new Date(2021, 3, 16), value: 3700 },
-        { date: new Date(2021, 3, 17), value: 3800 },
-        { date: new Date(2021, 3, 18), value: 3200 },
-        { date: new Date(2021, 3, 19), value: 3500 },
-        { date: new Date(2021, 3, 20), value: 4500 },
-        { date: new Date(2021, 3, 21), value: 4700 }
-      ]
-    }
-  ]
-
-  sessionsVsRetVisitor = [
-    { category: 'Visitantes nuevos', value: 3200 },
-    { category: 'Visitante recurrentes', value: 2800 }
-  ]
+export class BehaviourWrapperComponent implements OnInit, OnDestroy {
 
   salesVsRetVisitor = [
     { category: 'Visitantes nuevos', value: 130 },
     { category: 'Visitante recurrentes', value: 240 }
-  ]
-
-  salesByConversionSource = [
-    { category: 'Google', value: 100 },
-    { category: 'Display', value: 30 },
-    { category: 'Social', value: 40 },
-    { category: 'Others', value: 10 }
-  ]
-
-  salesBySector = [
-    { category: 'Search', value: 30 },
-    { category: 'Marketing', value: 240 },
-    { category: 'Ventas', value: 60 }
-  ]
+  ];
 
   sessionsByAudience = [
     { category: 'Prospecting', value: 49500 },
     { category: 'Remarketing', value: 15400 },
-  ]
+  ];
 
   quantityByAudience = [
     { category: 'Prospecting', value: 357 },
     { category: 'Remarketing', value: 18 },
-  ]
+  ];
 
   revenueVsAupAudience = [
     { category: 'Prospecting', revenue: 1200, aup: 400 },
     { category: 'Remarketing', revenue: 1600, aup: 810 }
+  ];
+
+  brVsPagesViewed: any[] = [];
+  brVsPagesViewedReqStatus: number = 0;
+
+  behavior = {};
+  behaviorReqStatus = [
+    { name: 'newUsersVsCurrent', reqStatus: 0 },
+    { name: 'salesBySource', reqStatus: 0 },
+    { name: 'salesBySector', reqStatus: 0 },
+    { name: 'sessionsByAudience', reqStatus: 0 },
   ]
 
+  generalFiltersSub: Subscription;
+  retailFiltersSub: Subscription;
 
-  constructor() { }
+  constructor(
+    private filtersStateService: FiltersStateService,
+    private campInRetailService: CampaignInRetailService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllData();
+
+    this.generalFiltersSub = this.filtersStateService.filtersChange$.subscribe(() => {
+      this.getAllData();
+    })
+
+    this.retailFiltersSub = this.filtersStateService.retailFiltersChange$.subscribe(() => {
+      this.getAllData();
+    });
   }
 
+  getAllData() {
+    this.getBounceRateAndPages();
+    this.getBehaviorMetrics();
+  }
+
+  getBounceRateAndPages() {
+    this.brVsPagesViewedReqStatus = 1;
+
+    this.campInRetailService.getDataByMetric('bouncerate-vs-pageviews').subscribe(
+      (resp: any[]) => {
+        this.brVsPagesViewed = resp;
+        this.brVsPagesViewedReqStatus = 2;
+      },
+      error => {
+        const errorMsg = error?.error?.message ? error.error.message : error?.message;
+        console.error(`[behavior.component]: ${errorMsg}`);
+        this.brVsPagesViewedReqStatus = 2;
+      });
+  }
+
+  getBehaviorMetrics() {
+    const requiredData = [
+      { subMetricType: 'new-users-vs-current', name: 'newUsersVsCurrent' },
+      { subMetricType: 'sales-by-source', name: 'salesBySource' },
+      { subMetricType: 'sales-by-sector', name: 'salesBySector' },
+      { subMetricType: 'sessions-by-audience', name: 'sessionsByAudience' },
+    ];
+
+    for (let subMetric of requiredData) {
+      const reqStatusObj = this.behaviorReqStatus.find(item => item.name === subMetric.name);
+      reqStatusObj.reqStatus = 1;
+
+      this.campInRetailService.getDataByMetric('behavior', subMetric.subMetricType).subscribe(
+        (resp: any[]) => {
+          this.behavior[subMetric.name] = resp;
+
+          reqStatusObj.reqStatus = 2;
+        },
+        error => {
+          const errorMsg = error?.error?.message ? error.error.message : error?.message;
+          console.error(`[behavior.component]: ${errorMsg}`);
+          reqStatusObj.reqStatus = 3;
+        });
+    }
+  }
+
+  ngOnDestroy() {
+    this.generalFiltersSub?.unsubscribe();
+    this.retailFiltersSub?.unsubscribe();
+  }
 }
