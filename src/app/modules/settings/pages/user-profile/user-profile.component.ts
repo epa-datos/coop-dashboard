9e41cfd99c239d282a8ac39a362a562fe9965dfa
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-07-06 15:18:15 -0500

    update user-profile component

--- src/app/modules/settings/pages/user-profile/user-profile.component.ts
+++ src/app/modules/settings/pages/user-profile/user-profile.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { catchError, map } from 'rxjs/operators';
 import { UserService } from 'src/app/services/user.service';
 import { EmailValidator } from 'src/app/tools/validators/email.validator';
-import { UserProfileService } from '../../services/user-profile.service';
-import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
-import { of } from 'rxjs';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit, OnDestroy {
+export class UserProfileComponent implements OnInit {
 
   form: FormGroup;
   name: AbstractControl;
@@ -28,15 +24,11 @@ export class UserProfileComponent implements OnInit, OnDestroy {
   };
 
   file;
-  fileContent: string | ArrayBuffer;
-  imgSub;
   avatarUrl;
-
 
   constructor(
     private fb: FormBuilder,
-    private userService: UserService,
-    private userProfileService: UserProfileService
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
@@ -63,11 +55,8 @@ export class UserProfileComponent implements OnInit, OnDestroy {
   }
 
   onFileSelected(fileList) {
-    console.log('onFileSelected', fileList)
     this.file = fileList[0];
-    console.log('file', this.file)
     const fileType = this.file.type.substring(this.file.type.indexOf('/') + 1, this.file.type.length).toUpperCase();
-    console.log('fileType', fileType)
 
     if (!this.imageValidators.fileType.includes(fileType)) {
       this.file.status = 'invalid';
@@ -78,13 +67,17 @@ export class UserProfileComponent implements OnInit, OnDestroy {
     this.file.status = 'loading';
     let formData: any = new FormData();
     formData.append('file', this.file);
-    console.log('formData', formData)
 
-    this.userProfileService.uploadProfileImage(formData).subscribe(
+    this.userService.uploadProfileImage(formData).subscribe(
       (res: any) => {
-        console.log('res', res);
         this.avatarUrl = res.url;
+        this.userService.userAvatarUrl = res.url;
         this.file.status = 'loaded';
+
+        // in order to clear generated legends
+        setTimeout(() => {
+          delete this.file;
+        }, 6000);
       },
       error => {
         this.file.status = 'failed';
@@ -96,42 +89,5 @@ export class UserProfileComponent implements OnInit, OnDestroy {
             : error;
         console.error(`[user-profile.component]: ${errorMsg}`);
       });
-
-    // const fileReader = new FileReader();
-    // fileReader.readAsArrayBuffer(this.file);
-    // fileReader.onload = () => {
-    //   this.fileContent = fileReader.result;
-    //   this.imgSub = this.userProfileService.uploadProfileImage(formData).pipe(
-    //     map((event) => {
-    //       switch (event.response.type) {
-    //         case HttpEventType.UploadProgress:
-    //           this.file.progress = Math.round(event.response.loaded * 100 / event.response.total);
-    //           console.log('fileProgress', this.file)
-    //           break;
-    //         case HttpEventType.Response:
-    //           this.file.publicURL = event.publicURL;
-    //           console.log('image finish!!!')
-    //           return event;
-    //       }
-
-    //       this.file.status = 'loading';
-    //     }),
-    //     catchError((error: HttpErrorResponse) => {
-    //       console.error(error);
-    //       this.file.status = 'failed';
-    //       this.file.error = error.statusText;
-    //       console.log('error', this.file.error)
-    //       // this.filesReqStatus = 3;
-    //       return of(error);
-    //     })).subscribe((event: any) => {
-    //       if (typeof (event) === 'object') {
-    //         // console.log({ event });
-    //       }
-    //     });
-    // }
-  }
-
-  ngOnDestroy() {
-    this.imgSub?.unsubscribe();
   }
 }
