8d43d137ef78050fc49e3dab08b58607ca87997c
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-07-27 22:53:35 -0500

    add campaign-comparator-filters component

---
+++ src/app/modules/dashboard/components/campaign-comparator-filters/campaign-comparator-filters.component.ts
@@ -0,0 +1,144 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsersMngmtService } from 'src/app/modules/users-mngmt/services/users-mngmt.service';
+import { AppStateService } from 'src/app/services/app-state.service';
+import { CampaignComparatorService } from '../../services/campaign-comparator.service';
+import { FiltersStateService } from '../../services/filters-state.service';
+
+@Component({
+  selector: 'app-campaign-comparator-filters',
+  templateUrl: './campaign-comparator-filters.component.html',
+  styleUrls: ['./campaign-comparator-filters.component.scss']
+})
+export class CampaignComparatorFiltersComponent implements OnInit {
+
+  @Output() selectedFiltersChange = new EventEmitter<{ retailer: any, campaign: any }>();
+
+  retailerList: any[];
+  campaignList: any[];
+
+  filteredRetailerList: any[];
+  filteredRetailer: boolean; // flag to know is filteredRetailerList is the result of a search filter
+  retailerFilter: string; // filtered value in filteredRetailerList
+
+  filteredCampaignList: any[];
+  filteredCampaign: boolean; // flag to know is campaignsList is the result of a search filter
+  campaignFilter: string; // filtered value in campaignsList
+
+  selectedRetailer: any;
+  selectedCampaign: any;
+
+  form: FormGroup;
+  retailer: AbstractControl;
+  campaign: AbstractControl;
+
+  retailersReqStatus: number = 2;
+  campaignsReqStatus: number = 0;
+
+  constructor(
+    private fb: FormBuilder,
+    private usersMngmtService: UsersMngmtService,
+    private campaignCompService: CampaignComparatorService,
+    private filtersStateService: FiltersStateService
+  ) { }
+
+  async ngOnInit() {
+    this.form = this.fb.group({
+      retailer: ['', [Validators.required]],
+      campaign: ['', [Validators.required]],
+    });
+
+    this.retailer = this.form.controls['retailer'];
+    this.campaign = this.form.controls['campaign'];
+
+
+    const savedRetailers = this.filtersStateService.retailersInitial;
+    if (savedRetailers?.length > 0) {
+      this.retailerList = savedRetailers;
+      this.filteredRetailerList = savedRetailers;
+    } else {
+      this.getRetailers();
+    }
+  }
+
+  getRetailers() {
+    this.retailersReqStatus = 1;
+    return this.usersMngmtService.getRetailers()
+      .toPromise()
+      .then((resp: any[]) => {
+        const retailers = resp.map(retailer => {
+          return { id: retailer.id, name: `${retailer.country_code} - ${retailer.name}` }
+        })
+
+        retailers.sort((a, b) => a.name.localeCompare(b.name));
+
+        this.retailerList = retailers;
+        this.filteredRetailerList = retailers;
+        this.filtersStateService.retailersInitial = retailers;
+
+        this.retailersReqStatus = 2;
+      })
+      .catch((error) => {
+        console.error(`[campaign-comparator-filter.component]: ${error}`);
+        this.retailersReqStatus = 3;
+      });
+  }
+
+  getCampaigns() {
+    this.campaignsReqStatus = 1;
+
+    return this.campaignCompService.getCampaigns(this.selectedRetailer.id)
+      .toPromise()
+      .then((res: any[]) => {
+        this.campaignList = res;
+        this.filteredCampaignList = res;
+
+        this.campaignsReqStatus = 2;
+      })
+      .catch(error => {
+        console.error(`[campaign-comparator-filter.component]: ${error}`);
+        this.campaignsReqStatus = 3;
+      });
+  }
+
+  filterFromList(elementRef: string, value: string) {
+    if (!value) {
+      return;
+    }
+    const pascalCaseRef = `${elementRef.charAt(0).toUpperCase()}${elementRef.slice(1)}`;
+
+    const arrayReference = `${elementRef}List`;
+    const filteredArrayRef = `filtered${pascalCaseRef}List`;
+
+    this[arrayReference] = this[filteredArrayRef].filter(item => {
+
+      return item.name.toLowerCase().includes(value.toLowerCase());
+    });
+  }
+
+  openedChange(elementRef: string, opened: boolean) {
+    // reset search without results if select is closed
+    const listRefPascalCase = `${elementRef.charAt(0).toUpperCase()}${elementRef.slice(1)}`;
+
+    const arrayReference = `${elementRef}List`;
+    const filteredArrayRef = `filtered${listRefPascalCase}List`;
+    const filterRef = `${elementRef}Filter`
+
+    if (!opened && this[elementRef] && this[arrayReference].length < 1) {
+      this[arrayReference] = [... this[filteredArrayRef]];
+      delete this[filterRef];
+    }
+  }
+
+  retailerChange() {
+    this.retailerFilter && delete this.retailerFilter;
+    this.getCampaigns();
+  }
+
+  campaignChange() {
+    this.campaignFilter && delete this.campaignFilter;
+
+    this.selectedFiltersChange.emit({ retailer: this.selectedRetailer, campaign: this.selectedCampaign });
+  }
+
+}
