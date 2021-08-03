6ca5e7f32974d31c5895fc0b04bbeb70db78a16a
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-08-03 14:58:07 -0500

    update campaign-comparator service

--- src/app/modules/dashboard/services/campaign-comparator.service.ts
+++ src/app/modules/dashboard/services/campaign-comparator.service.ts
@@ -2,6 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { Configuration } from 'src/app/app.constants';
+import { FiltersStateService } from './filters-state.service';
+import * as moment from 'moment';
+import { AUDIENCES, MEDIUMS, SOURCES } from 'src/app/tools/constants/filters';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +15,28 @@ export class CampaignComparatorService {
   constructor(
     private http: HttpClient,
     private config: Configuration,
+    private filtersStateService: FiltersStateService
   ) {
     this.baseUrl = this.config.endpoint;
+  }
+
+  /**
+ * Concated query params
+ * @returns query params 
+ */
+  concatedQueryParams(campaignID?: number): string {
+    const daysAgo = 90;
+    const startDate = moment().subtract(daysAgo, 'd').format('YYYY-MM-DD');
+    const endDate = moment().subtract(1, 'd').format('YYYY-MM-DD');
+
+    const sectors = this.filtersStateService.sectorsQParams;
+    const categories = this.filtersStateService.categoriesQParams;
+    const sourcesList = SOURCES.filter(item => item.id !== 'banner' && item.id !== 'institucional')
+    const sources = this.filtersStateService.convertArrayToQueryParams(sourcesList, 'id');
+    const mediums = this.filtersStateService.convertArrayToQueryParams(MEDIUMS, 'id');
+    const audiences = this.filtersStateService.convertArrayToQueryParams(AUDIENCES, 'id');
+
+    return `start_date=${startDate}&end_date=${endDate}&sectors=${sectors}&categories=${categories}&sources=${sources}&mediums=${mediums}&audiences=${audiences}${campaignID ? `&campaigns=${campaignID}` : ''}`;
   }
 
   getCampaigns(retailerID: number) {
@@ -21,7 +44,9 @@ export class CampaignComparatorService {
       return throwError('[campaign-comparator.service]: not retailerID provided');
     }
 
-    return this.http.get(`${this.baseUrl}/retailers/${retailerID}/campaigns`);
+    const queryParams = this.concatedQueryParams();
+
+    return this.http.get(`${this.baseUrl}/retailers/${retailerID}/campaigns?${queryParams}`);
   }
 
   getCampKpis(retailerID: number, campaignID: number) {
@@ -33,7 +58,9 @@ export class CampaignComparatorService {
       return throwError('[campaign-comparator.service]: not campaignID provided');
     }
 
-    return this.http.get(`${this.baseUrl}/retailers/${retailerID}/campaign-comparison/${campaignID}/kpis`);
+    const queryParams = this.concatedQueryParams(campaignID);
+
+    return this.http.get(`${this.baseUrl}/retailers/${retailerID}/kpis?${queryParams}`);
   }
 
   getCampAcquisition(retailerID: number, campaignID: number) {
@@ -45,7 +72,9 @@ export class CampaignComparatorService {
       return throwError('[campaign-comparator.service]: not campaignID provided');
     }
 
-    return this.http.get(`${this.baseUrl}/retailers/${retailerID}/campaign-comparison/${campaignID}/acquisition`);
+    const queryParams = this.concatedQueryParams(campaignID);
+
+    return this.http.get(`${this.baseUrl}/retailers/${retailerID}/in-retail/campaigns?${queryParams}`);
   }
 
   getCampConversion(retailerID: number, campaignID: number) {
@@ -57,6 +86,8 @@ export class CampaignComparatorService {
       return throwError('[campaign-comparator.service]: not campaignID provided');
     }
 
-    return this.http.get(`${this.baseUrl}/retailers/${retailerID}/campaign-comparison/${campaignID}/conversion`);
+    const queryParams = this.concatedQueryParams(campaignID);
+
+    return this.http.get(`${this.baseUrl}/retailers/${retailerID}/in-retail/conversions/products?${queryParams}`);
   }
 }
