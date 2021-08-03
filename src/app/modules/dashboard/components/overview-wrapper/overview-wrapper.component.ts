268168b395d69c21a58d332958b59a508fb3da80
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-08-03 14:57:25 -0500

    use sources constant in overview-wrapper component

--- src/app/modules/dashboard/components/overview-wrapper/overview-wrapper.component.ts
+++ src/app/modules/dashboard/components/overview-wrapper/overview-wrapper.component.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { Observable, Subscription } from 'rxjs';
 import { KpiCard } from 'src/app/models/kpi';
 import { AppStateService } from 'src/app/services/app-state.service';
+import { SOURCES } from 'src/app/tools/constants/filters';
 import { disaggregatePictorialData } from 'src/app/tools/functions/chart-data';
 import { convertWeekdayToString } from 'src/app/tools/functions/data-convert';
 import { FiltersStateService } from '../../services/filters-state.service';
@@ -192,15 +193,7 @@ export class OverviewWrapperComponent implements OnInit, OnDestroy {
 
     } else if (this.selectedType === 'retailer') {
       // for retailer view all valid sources are used because sources filter isn't shown 
-      this.selectedSources = [
-        { id: 'google', name: 'Google' },
-        { id: 'social', name: 'Social' },
-        { id: 'email', name: 'Email' },
-        { id: 'display', name: 'Display' },
-        { id: 'banner', name: 'Banner' },
-        { id: 'institucional', name: 'Institucional' },
-        { id: 'others', name: 'Otros' }
-      ];
+      this.selectedSources = SOURCES;
     }
 
     let selectedSectorHM;
